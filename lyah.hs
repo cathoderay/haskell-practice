9e1b7997fd81b6cda2b34dcb1095e9f1fc2c5132
Ronald Kaiser <raios.catodicos@gmail.com>
2011-09-25 11:29:52 -0300

    more recursive functions

--- lyah.hs
+++ lyah.hs
@@ -131,3 +131,15 @@ take' n _
 take' _ [] = []
 take' n (x:xs) = x : take' (n - 1) xs
 
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = (reverse' xs) ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+zip' ::  [a] -> [b] -> [(a, b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x, y):(zip' xs ys)
+
