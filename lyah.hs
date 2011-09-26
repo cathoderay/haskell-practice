bddbae8a9186be50002172c4a87c0df0601727ab
Ronald Kaiser <raios.catodicos@gmail.com>
2011-09-25 22:29:11 -0300

    qsort in haskell

--- lyah.hs
+++ lyah.hs
@@ -3,6 +3,7 @@ addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
 addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 -}
 
+
 first :: (a, b, c) -> a
 first (x, _, _) = x
 
@@ -10,8 +11,10 @@ first (x, _, _) = x
 second :: (a, b, c) -> b
 second (_, y, _) = y
 
+
 third :: (a, b, c) -> c
 third (_, _, z) = z
+
 
 {-
 head' :: [a] -> a
@@ -19,11 +22,13 @@ head' [] = error "Can't call head on an empty list, dummy!"
 head' (x:_) = x
 -}
 
+
 tell :: (Show a) => [a] -> String
 tell [] = "The list is empty"
 tell (x:[]) = "The list has one element: " ++ show x 
 tell (x:y:[]) = "The list has two elements : " ++ show x ++ " and " ++ show y
 tell (x:y:_) = "This list is long. The first two elements are : " ++ show x ++ " and " ++ show y
+
 
 badAdd :: (Num a) => [a] -> a
 badAdd (x:y:z:_) = x + y + z
@@ -32,6 +37,7 @@ badAdd (x:y:z:_) = x + y + z
 firstLetter :: String -> String
 firstLetter "" = "Empty string, whoops!"
 firstLetter all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
 
 {-
 bmiTell ::  Double -> String
@@ -42,6 +48,7 @@ bmiTell bmi
  | otherwise = "You're a whale, congratulations!"
 -}
 
+
 bmiTell :: Double -> Double -> String
 bmiTell weight height
  | bmi <= skinny  = "You're underweight, you emo, you!"
@@ -51,16 +58,19 @@ bmiTell weight height
  where bmi = weight / height ^ 2
        (skinny, normal, fat) = (18.5, 25.0, 30.0)
 
+
 max' :: (Ord a) => a -> a -> a
 max' a b
   | a <= b = b
   | otherwise = a
+
 
 myCompare :: (Ord a) => a -> a -> Ordering
 a `myCompare` b
  | a == b    = EQ
  | a <= b    = LT
  | otherwise = GT
+
 
 badGreeting :: String
 badGreeting = "Oh! Pfft. It's you."
@@ -73,17 +83,22 @@ greet "Juan" = niceGreeting ++ " Juan!"
 greet "Fernando" = niceGreeting ++ " Fernando!"
 greet name = badGreeting ++ " " ++ name
 
+
 {-
 initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
  where (f:_) = firstname
        (l:_) = lastname
 -}
+
+
 initials :: String -> String -> String
 initials (f:_) (l:_) = [f] ++ ". " ++ [l] ++ "."
+
 
 calcBmis :: [(Double, Double)] -> [Double]
 calcBmis xs = [bmi w h | (w, h) <- xs]
   where bmi weight height = weight / height ^ 2
+
 
 cylinder :: Double -> Double -> Double
 cylinder r h = 
@@ -91,20 +106,25 @@ cylinder r h =
       topArea = pi * r ^ 2
   in sideArea + 2 * topArea
 
+
 {-
 calcBmis :: [(Double, Double)] -> [Double]
 calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
 -}
 
+
 head' :: [a] -> a
 head' xs = case xs of [] ->  error "No head for empty lists!"
                       (x:_) -> x
+
+
 {-
 describeList :: [a] -> String                      
 describeList ls = "THe list is " ++ case ls of [] -> "empty."
                                                [x] -> "a singleton list."
                                                xs -> "a longer list."
 -}
+
 
 describeList :: [a] -> String                                               
 describeList ls = "The list is " ++ what ls
@@ -131,17 +151,21 @@ take' n _
 take' _ [] = []
 take' n (x:xs) = x : take' (n - 1) xs
 
+
 reverse' :: [a] -> [a]
 reverse' [] = []
 reverse' (x:xs) = (reverse' xs) ++ [x]
 
+
 repeat' :: a -> [a]
 repeat' x = x:repeat' x
+
 
 zip' ::  [a] -> [b] -> [(a, b)]
 zip' _ [] = []
 zip' [] _ = []
 zip' (x:xs) (y:ys) = (x, y):(zip' xs ys)
+
 
 elem' :: Eq(a) => a -> [a] -> Bool
 elem' x [] = False
@@ -149,4 +173,9 @@ elem' x (y:ys)
  | x == y = True
  | otherwise = x `elem'` ys
 
+
+qsort' :: Ord(a) => [a] -> [a]
+qsort' [] = []
+qsort' [x] = [x]
+qsort' (p:xs) = qsort' [x | x <- xs, x < p] ++ [p] ++ qsort' [x | x <- xs, x >= p]
 
