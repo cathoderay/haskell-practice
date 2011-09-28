c330f3d60a6ad628f7babdb7b9d41952fce5aded
Ronald Kaiser <raios.catodicos@gmail.com>
2011-09-28 18:06:20 -0300

    filter

--- lyah.hs
+++ lyah.hs
@@ -207,3 +207,11 @@ map' _ [] = []
 map' f (x:xs) = (f x):(map' f xs)
 
 
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x:xs)
+ | f x       = x:filter' f xs
+ | otherwise = filter f xs
+
+
+
