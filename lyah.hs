aaf6a445e7248a5e31b0e02680168bd0991469ef
Ronald Kaiser <raios.catodicos@gmail.com>
2011-09-28 17:12:48 -0300

    flip'

--- lyah.hs
+++ lyah.hs
@@ -195,4 +195,8 @@ applyTwice f x = f (f x)
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ _ [] = []
 zipWith' _ [] _ = []
-zipWith' f (x:xs) (y:ys) = (f x y):(zipWith' f xs ys )
+zipWith' f (x:xs) (y:ys) = (f x y):(zipWith' f xs ys)
+
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f x y = f y x
