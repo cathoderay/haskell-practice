7207f1349d305396f03057ea11d4de230b132e90
Ronald Kaiser <raios.catodicos@gmail.com>
2011-09-28 16:56:56 -0300

    zipWith

--- lyah.hs
+++ lyah.hs
@@ -191,3 +191,8 @@ isUpperAlphanum = (`elem` ['A'..'Z'])
 applyTwice :: (a ->  a) -> a -> a
 applyTwice f x = f (f x)
 
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ _ [] = []
+zipWith' _ [] _ = []
+zipWith' f (x:xs) (y:ys) = (f x y):(zipWith' f xs ys )
