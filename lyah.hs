97e79f35a0d1385c79e29d84059f0794a8027f5b
Ronald Kaiser <raios.catodicos@gmail.com>
2011-10-01 14:54:44 -0300

    filtering with foldr

--- lyah.hs
+++ lyah.hs
@@ -278,6 +278,11 @@ reverse''' = foldl (\acc x -> x:acc) []
 reverse'''' :: [a] -> [a]
 reverse'''' = foldl (flip (:)) []
 
+
 product' :: Num(a) => [a] -> a
 product' = foldl (*) 1
+
+
+filter'' :: (a -> Bool) -> [a] -> [a]
+filter'' f = foldr (\x acc -> if f x then x:acc else acc) []
 
