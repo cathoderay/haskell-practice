13c2fa1cdd1a2b923b9243ae9e866baa08a30a9e
Ronald Kaiser <raios.catodicos@gmail.com>
2011-10-02 19:37:57 -0300

    refactoring, loose coupling

--- caesar.hs
+++ caesar.hs
@@ -5,3 +5,9 @@ encode :: Int -> String -> String
 encode offset msg = map (chr . (+ offset) . ord) msg
 
 
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
+
+
+decode' :: Int -> String -> String
+decode' shift msg = map (\c -> chr $ (ord c) - shift) msg
