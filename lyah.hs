67b357debc3b7aadfd075085d237b3d479fa6efd
Ronald Kaiser <raios.catodicos@gmail.com>
2011-10-02 13:14:28 -0300

    $ from prelude disambiguation

--- lyah.hs
+++ lyah.hs
@@ -304,7 +304,7 @@ sqrtSums :: Int
 sqrtSums = length (takeWhile (< 1000) (scanl1 (+) (map sqrt [1..]))) + 1
 
 
-($) :: (a -> b) -> a -> b
-f $ x = f x
+($*) :: (a -> b) -> a -> b
+f $* x = f x
 
 
