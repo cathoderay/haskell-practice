170a802582555fa2c142989ed01cb728ec2b23c8
Ronald Kaiser <raios.catodicos@gmail.com>
2011-10-02 18:30:57 -0300

    playing with Data.List

---
+++ dataListPractice.hs
@@ -0,0 +1,9 @@
+import Data.List (group, sort, group, tails, isPrefixOf)
+
+
+wordNums :: String -> [(String,Int)]
+wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
+
+
+isIn :: (Eq a) => [a] -> [a] -> Bool
+needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
--- wordNums.hs
+++
@@ -1,5 +0,0 @@
-import Data.List (group, sort, group)
-
-
-wordNums :: String -> [(String,Int)]
-wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
