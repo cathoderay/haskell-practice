fb8ee163398c9f36e0d5757ffd1b2cc88f3e8984
Ronald Kaiser <raios.catodicos@gmail.com>
2011-10-11 20:45:26 -0300

    using Data.Map

---
+++ Map.hs
@@ -0,0 +1,12 @@
+{-- playing with Maps --}
+import qualified Data.Map as Map
+
+phoneBook :: Map.Map String String
+phoneBook = Map.fromList $
+    [("betty", "555-2938")
+    ,("bonnie", "452-2928")
+    ,("patsy", "493-2928")
+    ,("lucille", "205-2928")
+    ,("wendy", "939-8282")
+    ,("penny", "853-2492")
+    ]    
