40122fba20dd99ee58293c71b12fffd0af8136a7
Ronald Kaiser <raios.catodicos@gmail.com>
2011-10-12 10:06:32 -0300

    fromListWith

--- Map.hs
+++ Map.hs
@@ -14,3 +14,19 @@ phoneBook = Map.fromList $
 
 string2digits :: String -> [Int]
 string2digits = map digitToInt . filter isDigit
+
+phoneBook2 = 
+    [("betty", "555-2938")
+    ,("betty", "342-2492")
+    ,("bonnie", "452-2928")
+    ,("patsy", "493-2928")
+    ,("patsy", "943-2929")
+    ,("patsy", "827-9162")
+    ,("lucille", "205-2928")
+    ,("wendy", "939-8282")
+    ,("penny", "853-2494")
+    ,("penny", "555-2111")
+    ]
+
+phoneBookToMap :: (Ord k) => [(k, a)] -> Map.Map k [a]
+phoneBookToMap xs = Map.fromListWith (++) $ map (\(k, v) -> (k, [v])) xs
